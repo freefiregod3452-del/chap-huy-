fd59dec537f37d9cf87ab63bed3ddb08c071ba61
freefiregod3452-del <freefiregod3452@gmail.com>
2026-08-26 23:01:56 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // បំពេញព័ត៌មាន Supabase របស់អ្នកនៅទីនេះ
-const SUPABASE_URL = "YOUR_SUPABASE_URL";
-const SUPABASE_ANON_KEY = "YOUR_SUPABASE_ANON_KEY";
+const SUPABASE_URL = "https://kgdcegrvwvjotlbrxqou.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_VHy2v7cOGKmr4laLvPWREg_UwcRUAjl";
